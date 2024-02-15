23ce2963648296e8708ff5691d763f9f60467985
James Absalem Lintu <jamesabsalem@gmail.com>
2024-02-15 22:00:45 +0600

    first commit

---
+++ src/app/features/category/add-category/add-category.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnDestroy } from '@angular/core';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-add-category',
+  templateUrl: './add-category.component.html',
+  styleUrl: './add-category.component.css'
+})
+export class AddCategoryComponent implements OnDestroy {
+
+  model: AddCategoryRequest;
+  private addCategorySubscription? : Subscription
+
+  constructor(private categoryService: CategoryService) {
+    this.model = {
+      name: '',
+      urlHandle: ''
+    }
+  }
+  
+
+  onFormSubmit(){
+    this.addCategorySubscription = this.categoryService.addCategory(this.model)
+    .subscribe({
+      next: (response) => {
+        console.log("This was successfull!");
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
+  }
+}
---
+++ src/app/features/category/services/category.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category.model';
+import {environment} from '../../../../environments/environment'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) { }
+
+  addCategory(model: AddCategoryRequest): Observable<void> {
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/Categories/CreateCategory`,model);
+  }
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/Categories/GetAllCategories`)
+  }
+}
